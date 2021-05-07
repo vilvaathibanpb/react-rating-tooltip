42074356b1bc0aa57cdf1d2f44810d89383ee706
Shivam Bhanushali <shivambhanushali@Shivams-MacBook-Air.local>
2021-05-07 12:48:54 +0530

    Bump version

--- src/Star.js
+++ src/Star.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 function Star(props) {
   const [isActive, setIsActive] = useState(props.selected);
