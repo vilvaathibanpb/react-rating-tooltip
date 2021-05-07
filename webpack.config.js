e7213018835fc5872998aae65a41d83f8e44d374
Shivam Bhanushali <shivambhanushali@Shivams-MacBook-Air.local>
2021-05-07 12:48:15 +0530

    Add external to prevent react minified in production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve('lib'),
-        filename: 'Rating.js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    }
-}
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve("lib"),
+    filename: "Rating.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  externals: {
+    react: "react",
+  },
+};
