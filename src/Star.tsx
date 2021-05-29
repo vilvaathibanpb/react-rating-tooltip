c748c96593225ce4f130b3fb9f569b84970aa573
Ankit Chouhan <ankitchouhan1020@gmail.com>
2021-05-29 12:49:32 +0530

    feat: lint src files

--- src/Star.tsx
+++ src/Star.tsx
@@ -2,54 +2,56 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const Star = (props: Props) => {
-    const [selected, setSelected] = useState(props.selected);
-    const [showTooltip, setShowTooltip] = useState(false);
+  const [selected, setSelected] = useState(props.selected);
+  const [showTooltip, setShowTooltip] = useState(false);
 
-    useEffect(() => {
-        setSelected(props.selected)
-    },
-        [props.selected]
-    );
+  useEffect(() => {
+    setSelected(props.selected);
+  }, [props.selected]);
 
-    const onHover = () => {
-        props.onHover(props.index);
-        setShowTooltip(true);
-    }
+  const onHover = () => {
+    props.onHover(props.index);
+    setShowTooltip(true);
+  };
 
-    const onMouseLeave = () => {
-        props.onMouseLeave();
-        setShowTooltip(false);
-    }
+  const onMouseLeave = () => {
+    props.onMouseLeave();
+    setShowTooltip(false);
+  };
 
-    const selectStar = () => {
-        props.selectStar(props.index);
-    }
+  const selectStar = () => {
+    props.selectStar(props.index);
+  };
 
-    return (
-        <div
-            key={props.key}
-            onClick={selectStar}
-            onMouseOver={onHover}
-            onMouseOut={onMouseLeave}
-            className="tp-container"
-        >
-            {selected ? props.ActiveComponent : props.InActiveComponent}
-            {showTooltip && props.tpText && <span className="tooltiptext" style={props.tooltipStyle}>{props.tpText}</span>}
-        </div>
-    );
-}
+  return (
+    <div
+      key={props.key}
+      onClick={selectStar}
+      onMouseOver={onHover}
+      onMouseOut={onMouseLeave}
+      className="tp-container"
+    >
+      {selected ? props.ActiveComponent : props.InActiveComponent}
+      {showTooltip && props.tpText && (
+        <span className="tooltiptext" style={props.tooltipStyle}>
+          {props.tpText}
+        </span>
+      )}
+    </div>
+  );
+};
 
 type Props = {
-    tooltipStyle: React.CSSProperties
-    tpText: React.ReactNode
-    selected: boolean
-    key: number,
-    onMouseLeave: () => void,
-    onHover: (index: number) => void,
-    index: number,
-    selectStar: (index: number) => void,
-    InActiveComponent: React.ReactNode,
-    ActiveComponent: React.ReactNode
-}
+  tooltipStyle: React.CSSProperties;
+  tpText: React.ReactNode;
+  selected: boolean;
+  key: number;
+  onMouseLeave: () => void;
+  onHover: (index: number) => void;
+  index: number;
+  selectStar: (index: number) => void;
+  InActiveComponent: React.ReactNode;
+  ActiveComponent: React.ReactNode;
+};
 
 export default Star;
